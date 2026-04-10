bcf1683125a850feb681e677b2722f84347faf29
TheSevenGreen <thesevengreen@gmail.com>
2026-04-10 19:13:07 +0300

    Refactor CORS handling to use applyCors function
    Refactor CORS header application into a reusable function.

--- index.js
+++ index.js
@@ -16,16 +16,24 @@ app.use(async (ctx, next) => {
 
 // Global CORS
 app.use(async (ctx, next) => {
-  for (const corsHeaderKey of Object.keys(corsHeaders)) {
-    ctx.response.headers.set(corsHeaderKey, corsHeaders[corsHeaderKey]);
-  }
+  const applyCors = () => {
+    for (const corsHeaderKey of Object.keys(corsHeaders)) {
+      ctx.response.headers.set(corsHeaderKey, corsHeaders[corsHeaderKey]);
+    }
+  };
+
+  applyCors();
 
   if (ctx.request.method === "OPTIONS") {
     ctx.response.status = Status.NoContent;
+    applyCors();
     return;
   }
 
   await next();
+
+  // re-apply after route handlers in case something overwrote headers
+  applyCors();
 });
 
 app.use(mainRouter.routes());
